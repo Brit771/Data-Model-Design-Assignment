443713625ae294b3102761c7c862a69a6c5a941f
Brit Lev <brit771@gmail.com>
2024-11-19 11:07:07 +0200

    fixed tables creation

--- create_table.sql
+++ create_table.sql
@@ -2,14 +2,14 @@ CREATE TABLE Riders (
     RiderId INT PRIMARY KEY AUTO_INCREMENT,
     FullName VARCHAR(100) NOT NULL,
     Email VARCHAR(100) NOT NULL,
-    PhoneNumber VARCHAR(15) UNIQUE NOT NULL
+    PhoneNumber VARCHAR(10) UNIQUE NOT NULL
 );
 
 CREATE TABLE Drivers (
     DriverID INT PRIMARY KEY AUTO_INCREMENT,
     FullName VARCHAR(100) NOT NULL,
     Email VARCHAR(100) UNIQUE,
-    PhoneNumber VARCHAR(20)UNIQUE NOT NULL,
+    PhoneNumber VARCHAR(10)UNIQUE NOT NULL,
     LicenseNumber VARCHAR(50) UNIQUE
 );
 
@@ -18,7 +18,7 @@ CREATE TABLE Vehicles (
     LicensePlate VARCHAR(20) UNIQUE,
     Make VARCHAR(50),
     Model VARCHAR(50),
-    VehicleType ENUM('SUV', 'Sedan', 'Minivan') DEFAULT 'sedan'
+    VehicleType ENUM('SUV', 'Sedan', 'Minivan') DEFAULT 'Sedan'
 );
 
 CREATE TABLE Orders (
@@ -41,7 +41,7 @@ CREATE TABLE Rides (
     EndTime DATETIME,
     Distance DECIMAL(10, 2), -- assuming distance is in kilometers or miles
     RideStatus ENUM('In Progress', 'Completed', 'Cancelled') DEFAULT 'In Progress',
-    TotalFare DECIMAL(10, 2) -- assuming TotalFare in NIS
+    TotalFare DECIMAL(10, 2), -- assuming TotalFare in NIS
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID) ON DELETE CASCADE,
     FOREIGN KEY (RiderID) REFERENCES Riders(RiderID) ON DELETE CASCADE,
     FOREIGN KEY (DriverID) REFERENCES Drivers(DriverID) ON DELETE CASCADE,
@@ -55,7 +55,7 @@ CREATE TABLE Payments (
     Amount DECIMAL(10, 2) NOT NULL,
     PaymentStatus ENUM('Pending', 'Completed', 'Failed') DEFAULT 'Pending',
     PaymentMethod ENUM('Credit Card', 'Cash') NOT NULL,
-    PaymentTime DATETIME DEFAULT CURRENT_TIMESTAMP
+    PaymentTime DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (RideID) REFERENCES Rides(RideID) ON DELETE CASCADE
     FOREIGN KEY (Amount) REFERENCES Rides(TotalFare) ON DELETE CASCADE
 );
