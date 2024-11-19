790b5d39bbbdccb8c78978f2fe12826e404d8c97
Brit Lev <brit771@gmail.com>
2024-11-19 14:02:35 +0200

    removed unnecessary comments

--- analysis_visualization_example.py
+++ analysis_visualization_example.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Improved Sample DataFrame with a broader range of time slots and locations
+# Sample DataFrame with a broader range of time slots and locations
 data = {
     'RideDate': ['2024-11-18', '2024-11-18', '2024-11-18', '2024-11-18', '2024-11-18', 
                  '2024-11-19', '2024-11-19', '2024-11-19', '2024-11-19', '2024-11-19'],
@@ -30,11 +30,11 @@ pivot_df = df.pivot_table(
 plt.figure(figsize=(10, 6))
 sns.heatmap(
     pivot_df,
-    cmap='YlGnBu',  # You can choose other color maps
-    annot=True,  # Annotates the cells with values
-    fmt='d',  # Integer format for annotation
-    linewidths=0.5,  # To separate cells slightly
-    cbar_kws={'label': 'Total Rides'}  # Label for the color bar
+    cmap='YlGnBu',  
+    annot=True,
+    fmt='d',  
+    linewidths=0.5,  
+    cbar_kws={'label': 'Total Rides'}  
 )
 
 # Customize the plot
